70ec69f68053adffda5b2175279d2ac0704fb451
Cameron Johnson <cameronjohnson@Camerons-MacBook-Air-2.local>
2024-09-18 16:37:12 -0600

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        String firstName = Console.ReadLine();
+
+        Console.WriteLine("\nWhat is your last name? ");
+        String lastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
