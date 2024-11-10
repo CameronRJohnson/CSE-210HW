8a3abb6705d5a44418bfcef526e64eeefe2968ea
Cameron Johnson <cameronjohnson@Camerons-MacBook-Air-2.local>
2024-11-10 16:18:59 -0700

    Completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List <Shape> shapes= new List <Shape> ();
+
+        Square square = new Square("Blue", 10);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 2, 3);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Yellow", 4);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes) {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
